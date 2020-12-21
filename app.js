5358eee380379fce878aef39e0c50b13e2e772a1
voyevoda <elisa9el@gmail.com>
2020-12-20 20:08:53 -0500

    Added displayProducts to UI class to create the dom element for each product using data fetched from json file

--- app.js
+++ app.js
@@ -34,7 +34,33 @@ class Products {
 }
 
 // display products
-class UI {}
+class UI {
+  displayProducts(products) {
+    let result = '';
+    products.forEach((product) => {
+      result += `
+        <!-- single product -->
+        <article class="product">
+          <div class="img-container">
+            <img
+              src=${product.image}
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart">add to bag</i>
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
+        </article>
+        <!-- end single product -->
+        `;
+    });
+
+    productsDOM.innerHTML = result;
+  }
+}
 
 // local storage
 class Storage {}
@@ -44,5 +70,5 @@ document.addEventListener('DOMContentLoaded', () => {
   const products = new Products();
 
   // get all products
-  products.getProducts().then((data) => console.log(data));
+  products.getProducts().then((products) => ui.displayProducts(products));
 });
